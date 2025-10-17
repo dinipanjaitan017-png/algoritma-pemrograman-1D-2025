b84584501c4ab242c3d54e44632dab012fa87cf0
dinipanjaitan017-png <dinipanjaitan017@gmail.com>
2025-10-17 09:20:54 +0700

    UTS_ALPRO1D_250441100153_DiniMasAyuPanjaitan

---
+++ UTS/uts.py
@@ -0,0 +1,50 @@
+# program rental motor  ancok dengan kreteria
+print("1.motor matic - Rp. 50.00.000")
+print("2. motor trail - Rp.100 00 000 ")
+print("3.motor sprot - Rp.75.00.000")
+total_barang = 0
+
+input("/pilihan barang dari (1-3)")
+input ("jumlah barang seleuruhnya")
+input
+#penyeleksian kondisi
+if pelihan = "1:"ProcessLookupError
+    harga = Rp.50 00.000 * jumlah:
+    print(f anda membeli {jumlah} motor matic Rp.50.00.000(harga)")
+elif pilihan = "2:"print
+    harga = Rp.100.00.000 * jumlah:
+    print("f anda membeli {jumlah}")motor trail Rp.100.00.00(harga)"
+
+elif pilihan = "3:"print
+harga = 75.00.000 * jumlah:
+    print("f anda membeli {jumlah} motor sprot (harga)")
+    else:
+        (barang tidak valid)
+
+input_lagi ("jika ada tambahan barang akan di ketik y/n(lower)")
+input_lagi! = y:
+break
+
+
+#perulangan sebelum diskon
+if = motor matic
+Diskon = 0,15
+  elif  = motor trail:
+    diskon = 0,10:
+ elif = motor matic
+    diskon = 0,5:
+    else:
+    diskon = 0
+input_lagi ("masih ada tambahan barang y/n(lower)")
+input_lagi =("n")
+break
+
+
+
+
+
+
+
+
+
+    
